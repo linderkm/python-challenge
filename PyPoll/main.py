249014dbe5e7331e2248a7334947b5ab1a3b54b0
Kevin <kevin.linder94@gmail.com>
2024-09-29 00:04:17 -0600

    updated comments in PyPoll main.py; updated references in README

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,43 +1,49 @@
 import os
 import csv
 
+#creating a path object (1)
 import_path = os.path.join('Resources', 'election_data.csv')
-
+#opening and reading a csv file ('election_data.csv') located in the PyPoll/Resources folder (1)
 with open (import_path, 'r') as datafile:
-
+    #creating a iterable csv object (1)
     csvreader = csv.reader(datafile, delimiter = ',')
+    #removing the first row (headers) in the csv, offloading it into a variable using next() (1)
     header = next(csvreader)
+    #offloading csvreader data into a list object that can be iterated over more than once (2)
     data = list(csvreader)
 
-
+#initializing variables, and setting their initial values.
 vote_count = 0
 candidate1_count = 0
 candidate2_count = 0
 candidate3_count = 0
 
 
-winner = ""
-
-
+#loop to go over each line of the csv data (3)
 for i in range(len(data)):
 
+    #conditional to check if string matches specific candidate's name. If there's a match, update the total vote count variable and respective candidate's counter variable. (4)
     if data[i][2] == "Charles Casper Stockham":
         candidate1_count += 1
         vote_count += 1
+    #conditional to check if string matches specific candidate's name. If there's a match, update the total vote count variable and respective candidate's counter variable. (4)
     elif data[i][2] == "Diana DeGette":
         candidate2_count += 1
         vote_count += 1
+    #conditional to check if string matches specific candidate's name. If there's a match, update the total vote count variable and respective candidate's counter variable. (4)
     elif data[i][2] == "Raymon Anthony Doane":
         candidate3_count += 1
         vote_count += 1
 
 
+#creating variables to store calculated percentage values
 candidate1_percentage = (candidate1_count / vote_count) * 100
 candidate2_percentage = (candidate2_count / vote_count) * 100
 candidate3_percentage = (candidate3_count / vote_count) * 100
 
+#initialzing variable to store the name of the winning candidate.
 winner = " "
-
+#conditional that compares the values of candidate  vote count variables (4)
 if candidate1_count > candidate2_count and candidate3_count:
     winner = "Charles Casper Stockham"
 elif candidate2_count > candidate1_count and candidate3_count:
@@ -45,12 +51,13 @@ elif candidate2_count > candidate1_count and candidate3_count:
 elif candidate3_count > candidate1_count and candidate2_count:
     winner = "Raymon Anthony Doane"
 
+#block of code that prints the results to the terminal.
 print(" ")
 print("Election Results")
 print("----------------------------------------")
-print(f"Total Votes: {vote_count}")
+print(f"Total Votes: {vote_count}") #using fstring to integrate a variable into the print statement (5)
 print("----------------------------------------")
-print(f"Charles Casper Stockham: {round(candidate1_percentage, 3)}% ({candidate1_count})")
+print(f"Charles Casper Stockham: {round(candidate1_percentage, 3)}% ({candidate1_count})") #rounding variables to three decimal points (6)
 print(f"Diana DeGette: {round(candidate2_percentage, 3)}% ({candidate2_count})")
 print(f"Raymon Anthony Doane: {round(candidate3_percentage, 3)}% ({candidate3_count})")
 print("----------------------------------------")
@@ -59,10 +66,10 @@ print("----------------------------------------")
 print(" ")
 
 
-
+#outputting the findings to analysis.txt, stored in python-challenge/PyPoll/analysis (7)
 output_path = os.path.join('Analysis', 'analysis.txt')
 with open (output_path, 'w') as file:
-    file.write("Election Results" + "\n")
+    file.write("Election Results" + "\n") #using "\n" to force the next line of text to be written on the next line in the .txt file (7)
     file.write("----------------------------------" + "\n")
     file.write(f"Total Votes: {vote_count}" + "\n")
     file.write("----------------------------------" + "\n")
--- README.md
+++ README.md
@@ -1,17 +1,42 @@
 # python-challenge
 
+This readme includes sources for both scripts required in this 
+assignment (main.py for PyBank) and main.py for PyPoll.
 
+The sources referenced below are linked to comments in the code, 
+indicated by their number (all references to (1) in the code refer to the source listed at (1) below).
+------------------------------------------------------------------------------------------------------------
 
-(1) Module 3; Class 2; Activity 8
+# Sources for PyBank
 
-(2) Module 3; Class 3; Activity 6
+(1) Module 3; Class 2; Activity 8 (read_csv_solution.py)
 
-(3) Module 3; Class 3; Activity 5
+(2) Module 3; Class 3; Activity 6 (functions_solution.py)
+
+(3) Module 3; Class 3; Activity 5 (comprehensions_solution.py)
 
 (4) https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
 
 (5) Used Google AI suggestions, based off of the following search query: "can csvreader object in python only be used once?"
 
 (6) https://stackoverflow.com/questions/354883/alternatives-for-returning-multiple-values-from-a-python-function
+
+(7) https://stackoverflow.com/questions/34010106/python-open-and-write-to-a-txt-file
+
+-----------------------------------------------------------------------------------------------------------
+
+# Sources for PyPoll
+
+(1) Module 3; Class 2; Activity 8 (read_csv_solution.py)
+
+(2) Used Google AI suggestions, based off of the following search query: "can csvreader object in python only be used once?"
+
+(3) Module 3; Class 2; Activity 2 (simple_loops_solution.py)
+
+(4) Module 3; Class 1; Activity 7 (conditionals_solution.py)
+
+(5) Module 3; Class 3; Activity 1 (cereal_solution.py)
+
+(6) (4) https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
 
 (7) https://stackoverflow.com/questions/34010106/python-open-and-write-to-a-txt-file
